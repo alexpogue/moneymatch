fcdf29c7aca94b4dc6bc7c4c60787d75a51cdced
Shuyang Sun <sunbuffett@gmail.com>
2014-03-29 17:10:49 -0500

    Solved the duplicated string problem in most cases. (we don't care about rare cases for now)

--- js/string_split_func.js
+++ js/string_split_func.js
@@ -10,39 +10,101 @@
  */
 function splitString(str, numParts) {
     // Array of all words
-    var wordArr = str.split(" ");
+    var wordArr = [];
+    wordArr = str.split(" ");
     // Final amount of parts we want.
-    var numParts_final = Math.min(numParts, wordArr.size);
+    var numParts_final = Math.min(numParts, wordArr.length);
     // How many words are in a part (average)
-    var numWordsInEachPart = wordArr.size/numParts_final;
+    var numWordsInEachPart_f = wordArr.length/numParts_final;
     // Final result to return
-    var strArr;
-    // Start appending strings to strArr
+    var strArr = [];
     for (var i = 0; i < numParts_final; ++i) {
         strArr[i] = "";
-        for (var j = 0; j < numWordsInEachPart; ++j) {
-            strArr[i] += wordArr[i * numWordsInEachPart + j];
-            if (j < numWordsInEachPart - 1 && 
-                i * numWordsInEachPart + j < wordArr.size - 1 ) {
-                strArr[i] += " ";
+    }
+    // Start appending strings to strArr
+    var i = 0;
+    var currentIndAtWordArr_f = numWordsInEachPart_f;
+    var currentIndAtStrArr = 0;
+    while (i < wordArr.length) { // Nested loop, but runs at O(n)
+        var currentNumOfWordsInPart = Math.round(currentIndAtWordArr_f - i);
+        for (var j = 0; j < currentNumOfWordsInPart; ++j, ++i) {
+            strArr[currentIndAtStrArr] += wordArr[i];
+            if (j < currentNumOfWordsInPart - 1 && i < wordArr.length - 1) {
+                strArr[currentIndAtStrArr] += " ";
             }
         }
+        currentIndAtStrArr++;
+        currentIndAtWordArr_f += numWordsInEachPart_f;
     }
     
-    // test code
-    for (var s in strArr) {
-        console.log(s);
-    }
+    // Handle duplicate error
+    solveDuplicatedStrProblem(strArr);
+    
+    // Debug code {
+    console.log(strArr);
+//    for (var i = 0; i < strArr.length; ++i) {
+//        console.log((i + 1) + ": " + strArr[i]);
+//    }
+    // }
     
     return strArr;
 }
 
-/** Helper methods to determin if there are duplicates in the strArr.
+/** Helper methods to solve the duplicated string problem. If there are duplicated strings in strArr, change one of the duplicated strings.
  *  @param strArr The array of strings need to be analized.
- *  @return Boolean value indicating if there are duplicates in the array or not.
  */
-function stringDuplicated(strArr) {
-    var strSet = new Set(strArr);
-    return set.size = strArr.size;
+function solveDuplicatedStrProblem(strArr) {
+    if (strArr.length <= 1 ) {
+        return;
+    }
+    var duplicated = false;
+    var duplicatedString;
+    var tempArr = strArr.slice(0);
+    tempArr.sort();
+    for (var i = 0; i < tempArr.length - 1; ++i) {
+        if (tempArr[i] === tempArr[i + 1]) {
+            duplicated = true;
+            duplicatedString = tempArr[i];
+            break;
+        }
+    }
+    
+    // Debug code {
+//    if (duplicatedString) {
+//        console.log(duplicatedString);
+//    }
+    // }
+    
+    // If there are duplicated string:
+    if (duplicatedString) {
+        var ind = strArr.indexOf(duplicatedString);
+        
+        if (duplicatedString && ind > 0) { // If it's not the first string in strArr
+            var tempChunk = strArr[ind - 1].split(" ");
+            if (tempChunk.length > 1) { // If there are more than one word in the previouse string:
+                strArr[ind] = tempChunk[tempChunk.length - 1] + " " +strArr[ind];
+                strArr[ind - 1] = strArr[ind - 1].substring(0, strArr[ind - 1].length - tempChunk[tempChunk.length - 1].length - 1);
+                duplicatedString = false;
+            }
+        }
+        
+        if (duplicatedString && ind < strArr.length - 1) { // If it's not the last string in strArr
+            var tempChunk = strArr[ind + 1].split(" ");
+            if (tempChunk.length > 1) { // If there are more than one word in the later string:
+                strArr[ind] = strArr[ind] + " " + tempChunk[0];
+                strArr[ind + 1] = strArr[ind + 1].substring(tempChunk[0].length + 1, strArr[ind + 1].length);
+                duplicatedString = false;
+            }
+        }
+            
+        if (duplicatedString) {
+            console.error("String contains duplicated string problem that is very hard to solve.");
+            return;
+        } else {
+            solveDuplicatedStrProblem(strArr);
+        }
+    } else {
+        return;
+    }
 }
 
